b07595942fc32a099f811396323a6838427000cc
Oleksandr Riadinskyi <aradinskij6@gmail.com>
2025-08-19 20:56:12 +0200

    Location views tests implementing

--- core/tests/api_test.py
+++ core/tests/api_test.py
@@ -6,12 +6,15 @@ from core.config import correct_token
 
 TEST_API_TOKEN = correct_token
 
+
 @pytest.mark.asyncio
 async def test_get_cached_alerts():
     async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test") as ac:
-        response = await ac.get("/api/v1/air-alert/cashed-alerts",
-                                headers={"X-API-Token": TEST_API_TOKEN})
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get(
+            "/api/v1/air-alert/cashed-alerts", headers={"X-API-Token": TEST_API_TOKEN}
+        )
         data = response.json()
         assert response.status_code == 200
         assert data["data"]
@@ -20,8 +23,54 @@ async def test_get_cached_alerts():
 @pytest.mark.asyncio
 async def test_get_active_alerts():
     async with AsyncClient(
-        transport=ASGITransport(app=app), base_url="http://test") as ac:
-        response = await ac.get("/api/v1/air-alert/codifier/",
-                                headers={"X-API-Token": TEST_API_TOKEN})
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get(
+            "/api/v1/air-alert/codifier/", headers={"X-API-Token": TEST_API_TOKEN}
+        )
         assert response.status_code == 200
 
+
+@pytest.mark.asyncio
+async def test_search_by_name():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test/api/v1/codifier"
+    ) as ac:
+        response = await ac.get(
+            "/search/by-name",
+            headers={"X-API-TOKEN": TEST_API_TOKEN},
+            params={"q": "Татарбунари"},
+        )
+        correct_ua_code = "UA51040250010015619"
+        data = response.json()
+        assert response.status_code == 200
+        assert data[0]["code"] == correct_ua_code
+
+
+@pytest.mark.asyncio
+async def test_search_ua_code():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://testapi/api/v1/codifier"
+    ) as ac:
+        dict_of_locations = {
+            "Київ": {"code": "UA80000000000093317", "category": "K"},
+            "Запорізька": {"code": "UA23000000000064947", "category": "O"},
+            "Ізмаїльський": {"code": "UA51080000000061776", "category": "P"},
+            "Моршинська": {"code": "UA46100130000048262", "category": "H"},
+            "Кривий Ріг": {"code": "UA12060170010065850", "category": "M"},
+            "Новоіванівка": {"code": "UA12060170020084226", "category": "C"},
+            "Гірницьке": {"code": "UA12060170050068450", "category": "X"},
+        }
+        for city_name, city_data in dict_of_locations.items():
+            print("\n", city_name)
+            print(city_data["code"])
+            response = await ac.get(
+                f"/search/by-code/{city_data['code']}",
+                headers={"X-API-TOKEN": TEST_API_TOKEN},
+            )
+            data = response.json()
+            print("data:", data)
+            print("last_chain:", data["chain"][-1])
+            assert response.status_code == 200
+            assert data["chain"][-1] == city_name
+            assert data["category"] == city_data["category"]
